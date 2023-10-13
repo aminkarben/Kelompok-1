dddcaae36b5dcc7f7496f2e596073daf513d72e3
Davincy Project <bdavincy.bd@gmail.com>
2023-10-13 19:11:58 +0700

    feature: added register template

--- src/Components/Auth/Register.jsx
+++ src/Components/Auth/Register.jsx
@@ -1,5 +1,82 @@
+import { Button, Form, Container, Card } from "react-bootstrap";
+import { useState } from "react";
+
 const Register = () => {
-    return <div>Pages for Register test</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  const registerAccount = async (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div className="d-flex min-vh-100 justify-content-center align-items-center text-black">
+      <Container className="p-4 w-75">
+        <Card>
+          <Card.Header>Register an Account</Card.Header>
+          <Card.Body>
+            <Form onSubmit={registerAccount}>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>First Name</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter Your First Name"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Last Name</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter Your Last Name"
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter Your email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Confirm Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Confirm Password"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+              </Form.Group>
+
+              <Button className="w-100" variant="danger" type="submit">
+                Register
+              </Button>
+            </Form>
+          </Card.Body>
+        </Card>
+      </Container>
+    </div>
+  );
 };
 
 export default Register;
