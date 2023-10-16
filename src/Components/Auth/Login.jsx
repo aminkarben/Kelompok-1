d14d898f2a1586d2592900994170ef9cfd7cdf8d
Davincy Project <bdavincy.bd@gmail.com>
2023-10-16 18:58:52 +0700

    changes: add protection for main pages before login

--- src/Components/Auth/Login.jsx
+++ src/Components/Auth/Login.jsx
@@ -3,37 +3,37 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 
 const Login = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const login = async (event) => {
-        event.preventDefault();
+  const login = async (event) => {
+    event.preventDefault();
 
-        try {
-            const response = await axios.post(
-                `${import.meta.env.VITE_VERCEL_AUTH}/login`,
-                {
-                    email,
-                    password,
-                }
-            );
-            const { data } = response.data;
-            const { token } = data;
-
-            localStorage.setItem("token", token);
-
-            window.location.replace("/");
-        } catch (error) {
-            if (axios.isAxiosError(error)) {
-                alert(error?.response?.data?.message);
-                return;
-            }
+    try {
+      const response = await axios.post(
+        `${import.meta.env.VITE_VERCEL_AUTH}/login`,
+        {
+          email,
+          password,
         }
-    };
+      );
+      const { data } = response.data;
+      const { token } = data;
 
-    return (
-        <div className="d-flex min-vh-100 justify-content-center align-items-center text-black">
-            {/* <Container className="p-4 w-100">
+      localStorage.setItem("token", token);
+
+      window.location.replace("/");
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        alert(error?.response?.data?.message);
+        return;
+      }
+    }
+  };
+
+  return (
+    <div className="d-flex min-vh-100 justify-content-center align-items-center text-black">
+      {/* <Container className="p-4 w-100">
                 <Card>
                     <Card.Body>
                         <h1>Sign in to your account</h1>
@@ -120,119 +120,113 @@ const Login = () => {
                 </Card>
             </Container> */}
 
-            <div
-                className="mt-5 px-4 py-5 px-md-5 text-center text-lg-start rounded mx-2"
-                style={{ backgroundColor: "hsl(0, 0%, 96%)" }}
-            >
-                <div className="container">
-                    <div className="row gx-lg-5 align-items-center">
-                        <div className="col-lg-6 mb-2 mb-lg-0">
-                            <h1 className="fs-1 my-4 display-3 fw-bold ls-tight">
-                                Welcome Back! <br />
-                                <span className="text-danger fs-4">
-                                    Log in to Access our Movie Collection
-                                </span>
-                            </h1>
-                            <p className="mb-4 text-danger opacity-50 fw-semibold">
-                                If you already have an account, please enter
-                                your login information to continue.
-                            </p>
-                        </div>
-
-                        <div className="col-lg-6 mb-5 mb-lg-0">
-                            <div className="card">
-                                <div className="card-body py-5 px-md-5">
-                                    <form onSubmit={login}>
-                                        <div className="form-outline mb-4">
-                                            <input
-                                                type="email"
-                                                id="form3Example3"
-                                                className="form-control"
-                                                placeholder="Email address"
-                                                value={email}
-                                                onChange={(e) =>
-                                                    setEmail(e.target.value)
-                                                }
-                                            />
-                                        </div>
-                                        <div className="form-outline mb-4">
-                                            <input
-                                                type="password"
-                                                id="form3Example3"
-                                                className="form-control"
-                                                placeholder="Password"
-                                                value={password}
-                                                onChange={(e) =>
-                                                    setPassword(e.target.value)
-                                                }
-                                            />
-                                        </div>
-                                        <div className="container w-full text-center">
-                                            <button
-                                                type="submit"
-                                                className="btn btn-danger btn-block mb-4 w-75"
-                                            >
-                                                Sign in
-                                            </button>
-                                        </div>
-
-                                        <p className="mb-3 fw-bold">
-                                            Don&apos;t have an account ?{" "}
-                                            <Link
-                                                to="/register"
-                                                className="text-primary-50 fw-bold text-decoration-none"
-                                            >
-                                                Sign Up
-                                            </Link>
-                                        </p>
-
-                                        <div className="text-center">
-                                            <div className="row">
-                                                <div className="col">
-                                                    <hr />
-                                                </div>
-                                                <div className="col-auto">
-                                                    or
-                                                </div>
-                                                <div className="col">
-                                                    <hr />
-                                                </div>
-                                            </div>
-
-                                            <button
-                                                type="button"
-                                                className="btn btn-link btn-floating mx-1"
-                                            >
-                                                <img
-                                                    src="/google.svg"
-                                                    style={{
-                                                        width: "30px",
-                                                        height: "30px",
-                                                    }}
-                                                />
-                                            </button>
-                                            <button
-                                                type="button"
-                                                className="btn btn-link btn-floating mx-1"
-                                            >
-                                                <img
-                                                    src="/facebook.svg"
-                                                    style={{
-                                                        width: "30px",
-                                                        height: "30px",
-                                                    }}
-                                                />
-                                            </button>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+      <div
+        className="mt-5 px-4 py-5 px-md-5 text-center text-lg-start rounded mx-2"
+        style={{ backgroundColor: "hsl(0, 0%, 96%)" }}
+      >
+        <div className="container">
+          <div className="row gx-lg-5 align-items-center">
+            <div className="col-lg-6 mb-2 mb-lg-0">
+              <h1 className="fs-1 my-4 display-3 fw-bold ls-tight">
+                Welcome Back! <br />
+                <span className="text-danger fs-4">
+                  Log in to Access our Movie Collection
+                </span>
+              </h1>
+              <p className="mb-4 text-danger opacity-50 fw-semibold">
+                If you already have an account, please enter your login
+                information to continue.
+              </p>
             </div>
+
+            <div className="col-lg-6 mb-5 mb-lg-0">
+              <div className="card">
+                <div className="card-body py-5 px-md-5">
+                  <form onSubmit={login}>
+                    <div className="form-outline mb-4">
+                      <input
+                        type="email"
+                        id="form3Example3"
+                        className="form-control"
+                        placeholder="Email address"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                      />
+                    </div>
+                    <div className="form-outline mb-4">
+                      <input
+                        type="password"
+                        id="form3Example3"
+                        className="form-control"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                      />
+                    </div>
+                    <div className="container w-full text-center">
+                      <button
+                        type="submit"
+                        className="btn btn-danger btn-block mb-4 w-75"
+                      >
+                        Sign in
+                      </button>
+                    </div>
+
+                    <p className="mb-3 fw-bold">
+                      Don&apos;t have an account ?{" "}
+                      <Link
+                        to="/register"
+                        className="text-primary-50 fw-bold text-decoration-none"
+                      >
+                        Sign Up
+                      </Link>
+                    </p>
+
+                    <div className="text-center">
+                      <div className="row">
+                        <div className="col">
+                          <hr />
+                        </div>
+                        <div className="col-auto">or</div>
+                        <div className="col">
+                          <hr />
+                        </div>
+                      </div>
+
+                      <button
+                        type="button"
+                        className="btn btn-link btn-floating mx-1"
+                      >
+                        <img
+                          src="/google.svg"
+                          style={{
+                            width: "30px",
+                            height: "30px",
+                          }}
+                        />
+                      </button>
+                      <button
+                        type="button"
+                        className="btn btn-link btn-floating mx-1"
+                      >
+                        <img
+                          src="/facebook.svg"
+                          style={{
+                            width: "30px",
+                            height: "30px",
+                          }}
+                        />
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
